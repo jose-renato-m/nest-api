e3e80231aca166e9d1b6383f6939fbf8bfa3ba42
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-09-26 19:00:43 -0300

    :sparkles: user creation operation finished

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { CreateUserInput } from './dtos/create-user.input';
+import { User } from './user.entity';
 
-@Resolver()
-export class UserResolver {}
+@Resolver('User')
+export class UserResolver {
+  constructor(
+    private userService: UserService
+  ) {}
+
+  @Query(() => [User])
+  async users(): Promise<User[]> {
+    const users = await this.userService.findAllUsers();
+    return users;
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Args('data') data: CreateUserInput
+  ): Promise<User> {
+    const user = await this.userService.createUser(data);
+    return user
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserInput } from './dtos/create-user.input';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+
+  async findAllUsers(): Promise<User[]> {
+    const users = await this.userRepository.find();
+    return users;
+  }
+
+  async createUser(data: CreateUserInput): Promise<User> {
+    const user = await this.userRepository.create(data);
+    const userSaved = await this.userRepository.save(user);
+
+    if(!userSaved) {
+      throw new InternalServerErrorException('Problema para criar um usuário.')
+    }
+
+    return userSaved;
+  }
+}
