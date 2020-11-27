7af4866700f0177c1315eb1d4f41a283bd070c6b
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-11-26 22:15:56 -0300

    fix:fix update

--- src/user/dtos/update-user.input .ts
+++ src/user/dtos/update-user.input .ts
@@ -6,13 +6,13 @@ import { IsString, IsNotEmpty, IsEmail, IsOptional } from "class-validator";
 export class UpdateUserInput {
   @IsString()
   @IsNotEmpty({ message: 'Este campo não pode estar vazio' })
-  @Field()
+  @Field({ nullable: true })
   @IsOptional()
   name?: string;
 
   @IsEmail()
   @IsNotEmpty({ message: 'Este campo não pode estar vazio' })
-  @Field()
+  @Field({ nullable: true })
   @IsOptional()
   email?: string;
 }
